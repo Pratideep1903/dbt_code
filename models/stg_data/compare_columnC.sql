55328a334a9037d5fe35b6253b3155d48320d731
Pratiksha Avhad <pratiksha.avhad1903@gmail.com>
2024-09-09 12:38:16 +0000

    commit

--- models/stg_data/compare_columnC.sql
+++ models/stg_data/compare_columnC.sql
@@ -1,2 +1,2 @@
 {{compare_count
-('DBT_SALES.dbt_source.customer_data','DBT_SALES.dbt_source.orders','DBT_SALES.dbt_source.payments')}}
+('DBT_SALES.dbt_source.customer_data','DBT_SALES.dbt_source.orders','DBT_SALES.dbt_source.emp')}}
